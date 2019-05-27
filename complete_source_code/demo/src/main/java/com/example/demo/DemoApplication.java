228967df4a061d22699d0fa5fccdd02c5325edcb
Teorell <fedorizmalkov@gmail.com>
2019-05-27 23:36:12 +0300

    1

---
+++ complete_source_code/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloWorld");
+
+        System.out.println(helloWorld.getHello()+ "\n");
+
+        HelloRussia helloRussia = (HelloRussia) applicationContext.getBean("helloRussia");
+        HelloIndia helloIndia = (HelloIndia) applicationContext.getBean("helloIndia");
+
+        helloRussia.sayHello();
+        helloIndia.sayHello();
+
+    }
+
+}
---
+++ complete_source_code/demo/src/main/java/com/example/demo/HelloRussia.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+public class HelloRussia {
+    private String intro;
+    private HelloWorld helloWorld;
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public HelloWorld getHelloWorld() {
+        return helloWorld;
+    }
+
+    public void setHelloWorld(HelloWorld helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
+    public void sayHello(){
+        System.out.println(helloWorld.getHello());
+        System.out.println(intro);
+        System.out.println();
+    }
+}
